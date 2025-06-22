df825c9213eac29107cd0192b229c4f01e918547
Rachel Kirkland <100070583+littleangelx@users.noreply.github.com>
2025-06-22 19:06:49 +0100

    move js into module

---
+++ index.js
@@ -0,0 +1,16 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://leejccnwdmgpzqcpajda.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+document
+  .getElementById("deleteForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+
+    const { data, error } = await supabase
+      .from("email_delete_requests")
+      .insert([{ email_address: email }])
+      .select();
+  });
